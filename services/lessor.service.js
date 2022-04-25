fa3b057355167eae93c6b0fd979f110310bedea9
Huynh Thanh Tam <huynhthanhtam2605@gmail.com>
2022-04-26 03:04:28 +0700

    feat: list article lessor

--- services/lessor.service.js
+++ services/lessor.service.js
@@ -6,48 +6,57 @@ const AppError = require('../utils/appError');
 const SendOTPService = require('./sendOTP.service');
 
 function generateRandomNumber() {
-  var minm = 100000;
-  var maxm = 999999;
-  return Math.floor(Math
-    .random() * (maxm - minm + 1)) + minm;
+    var minm = 100000;
+    var maxm = 999999;
+    return Math.floor(Math.random() * (maxm - minm + 1)) + minm;
 }
 const identifyPhoneNumber = async (lessorBody) => {
-  const otp = generateRandomNumber();
-  const body = {
-    message: `${otp} là mã OTP của bạn. Vui lòng không cung cấp OTP này cho người khác`,
-    phoneNumber: lessorBody.phoneNumber
-  }
-  const sendOTP = await SendOTPService.sendSMS(body);
-  return {
-    message: sendOTP,
-    otp: Number(otp),
-  }
-}
+    const otp = generateRandomNumber();
+    const body = {
+        message: `${otp} là mã OTP của bạn. Vui lòng không cung cấp OTP này cho người khác`,
+        phoneNumber: lessorBody.phoneNumber,
+    };
+    const sendOTP = await SendOTPService.sendSMS(body);
+    return {
+        message: sendOTP,
+        otp: Number(otp),
+    };
+};
 
 const createLessor = async (id) => {
-  const lessor = {
-    lessorId: id
-  }
-  await Account.updateOne({ _id: id }, { role: 'lessor' });
-  return Lessor.create(lessor);
+    const lessor = {
+        lessorId: id,
+    };
+    await Account.updateOne({ _id: id }, { role: 'lessor' });
+    return Lessor.create(lessor);
 };
 
 const getListArticle = async (data) => {
-  const page = data.page || 1;
-  const limit = data.page || 10;
-  const skip = (page - 1) * limit;
-  const articles = await Article.find({ lessor: data.id })
-    .skip(skip)
-    .limit(limit)
-    .exec()
-  const total = await Article.find({lessor: data.id}).count();
-  return {
-    articles,
-    total
-  }
-}
+    const { page, limit, id, keyword, ...rest } = data;
+    const currentPage = page || 1;
+    const currentLimit = limit || 10;
+    const skip = (currentPage - 1) * currentLimit;
+    const articles = await Article.find({
+        lessor: id,
+        title: { $regex: keyword, $options: 'i' },
+        ...rest,
+    })
+        .skip(skip)
+        .limit(currentLimit)
+        .exec();
+    const total = await Article.find({
+        lessor: id,
+        title: { $regex: keyword, $options: 'i' },
+        ...rest,
+    }).count();
+    return {
+        articles,
+        total,
+    };
+};
+
 module.exports = {
-  identifyPhoneNumber,
-  createLessor,
-  getListArticle
+    identifyPhoneNumber,
+    createLessor,
+    getListArticle,
 };
