3ad4fda1d4a2ab932484c43e5534fdd19ca632b4
Nguyen Thi Nhu Quynh <67548525+quynhbud@users.noreply.github.com>
2022-05-24 05:23:21 +0700

    fix payment

--- services/lessor.service.js
+++ services/lessor.service.js
@@ -57,113 +57,11 @@ const getListArticle = async (data) => {
     };
 };
 
-const updateMemberPackage = async (data) => {
-    const memberPackage = await MemberPackage.findById(data.memberPackageId);
-    const updateData = {
-        memberPackageId: data.memberPackageId,
-        articleTotal: memberPackage.articlePerMonth,
-        articleUsed: 0,
-    };
-    await Lessor.updateOne({ account: data.accountId }, updateData);
-    const updateLessor = await Lessor.findOne({account: data.accountId});
-    return {
-        data: updateLessor,
-        message: 'Cập nhật gói thành viên người cho thuê thành công',
-    };
-}
-const paymentMemberPackage = async (req, lessorId, data) => {
-    const saveData = {
-        lessorId: lessorId,
-        memberPackageId: data.memberPackageId,
-        transactionAmount: data.prices,
-        status: 'WAITFORPAYMENT',
-    }
-    const transaction = await MemberPackageTransaction.create(saveData);
-    const orderDescription = `${data.memberPackageId}-${transaction._id}-${lessorId}`
-    const transactionData = {
-        typeOrders: "payment",
-        amount: `${~~data.prices}`,
-        bankCode: "NCB",
-        orderDescription: orderDescription,
-        language: "vn",
-        typeCart: 'CLIENT',
-        //servicePackage: data.servicePackageId,
-        lessor: lessorId,
-    };
-    const resultPayment = await VNPayService.payment(
-        req,
-        transactionData,
-    );
-    if (resultPayment.success) {
-        return {
-            success: true,
-            message: "Thanh toán thành công",
-            data: resultPayment.data.url,
-            status: 200,
-        };
-    } else {
-        return {
-            success: false,
-            message: "Thanh toán thất bại",
-            data: resultPayment.data.url,
-            status: 200,
-        };
-    }
-}
-const savePaymentResult = async(data) => {
-    try {
-        let vnp_Params = {
-            vnp_Amount: data.vnp_Amount,
-            vnp_BankCode: data.vnp_BankCode,
-            vnp_BankTranNo: data.vnp_BankTranNo,
-            vnp_CardType: data.vnp_CardType,
-            vnp_OrderInfo: data.vnp_OrderInfo,
-            vnp_PayDate: data.vnp_PayDate,
-            vnp_ResponseCode: data.vnp_ResponseCode,
-            vnp_TmnCode: data.vnp_TmnCode,
-            vnp_TransactionNo: data.vnp_TransactionNo,
-            vnp_TransactionStatus: data.vnp_TransactionStatus,
-            vnp_TxnRef: data.vnp_TxnRef,
-            vnp_SecureHashType: data.vnp_SecureHashType,
-            vnp_SecureHash: data.vnp_SecureHash,
-        };
-        const memberPackageId = vnp_Params.vnp_OrderInfo.split('-')[0];
-        const transactionId = vnp_Params.vnp_OrderInfo.split('-')[1];
-        const accountId = vnp_Params.vnp_OrderInfo.split('-')[2];
-        const reqData = {
-            memberPackageId,
-            transactionId,
-            accountId,
-        }
-        if (Number(vnp_Params.vnp_TransactionStatus) === 0) {
-            const updateLessor = await updateMemberPackage(reqData);
-            const updateTransaction = await MemberPackageTransaction.updateOne({ _id: transactionId }, { status: 'SUCCESS' })
-            return {
-                success: true,
-                data: updateLessor.data,
-                message: "Thanh toán thành công",
-                status: 200,
-            }
-        }
-        return {
-            success: false,
-            message: "Thanh toán thất bại",
-            status: 300,
-        }
-    } catch {
-        return {
-            success: false,
-            message: "Thanh toán thất bại",
-            status: 500,
-        }
-    }
-}
+
+
 
 module.exports = {
     identifyPhoneNumber,
     createLessor,
     getListArticle,
-    paymentMemberPackage,
-    savePaymentResult,
-    updateMemberPackage,
 };
