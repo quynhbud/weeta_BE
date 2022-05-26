b87b2b2e8615f29b9a37215542c68f3ccb3d41a9
Huynh Thanh Tam <huynhthanhtam2605@gmail.com>
2022-05-26 11:47:11 +0700

    fix: thanh toan thanh cong ? query

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,36 +1,39 @@
 const catchAsync = require('../utils/catchAsync');
-const  PaymentPackageService = require('../services/payment.service');
+const PaymentPackageService = require('../services/payment.service');
 const { sendSuccess, sendError } = require('./return.controller');
 
 const paymentPackage = catchAsync(async (req, res) => {
-  const lessorId = req.user._id;
-  const result = await PaymentPackageService.paymentPackage(req, lessorId, req.body);
-  if (result.status != 200) {
-    return sendError(res, result.status, result.message);
-  }
-  return sendSuccess(res, result.data, result.status, result.message);
-})
+    const lessorId = req.user._id;
+    const result = await PaymentPackageService.paymentPackage(
+        req,
+        lessorId,
+        req.body
+    );
+    if (result.status != 200) {
+        return sendError(res, result.status, result.message);
+    }
+    return sendSuccess(res, result.data, result.status, result.message);
+});
 
 const savePaymentResult = catchAsync(async (req, res) => {
-  const result = await PaymentPackageService.savePaymentResult(req.query);
-  if (result.status != 200) {
-    return sendError(res, result.status, result.message);
-  }
-  const {type,articleId, paymentId} = result;
-  if(type === 'SERVICEPACKAGE'){
-    res.writeHead(301, {
-      Location: `https://weeta-housing.vercel.app/thanh-toan-thanh-cong/type=SERVICEPACKAGE&articleId=${articleId}&paymentId=${paymentId}`
-    }).end();
-  }
-  if(type === 'MEMBERPACKAGE') {
-    res.writeHead(301, {
-      Location: `https://weeta-housing.vercel.app/thanh-toan-thanh-cong/type=MEMBERPACKAGE&paymentId=${paymentId}`
-    }).end();
-  }
-  
-})
+    const result = await PaymentPackageService.savePaymentResult(req.query);
+    if (result.status != 200) {
+        return sendError(res, result.status, result.message);
+    }
+    const { type, articleId, paymentId } = result;
+    if (type === 'SERVICEPACKAGE') {
+        res.writeHead(301, {
+            Location: `https://weeta-housing.vercel.app/thanh-toan-thanh-cong/?type=SERVICEPACKAGE&articleId=${articleId}&paymentId=${paymentId}`,
+        }).end();
+    }
+    if (type === 'MEMBERPACKAGE') {
+        res.writeHead(301, {
+            Location: `https://weeta-housing.vercel.app/thanh-toan-thanh-cong/?type=MEMBERPACKAGE&paymentId=${paymentId}`,
+        }).end();
+    }
+});
 
 module.exports = {
-  paymentPackage,
-  savePaymentResult
-}
+    paymentPackage,
+    savePaymentResult,
+};
